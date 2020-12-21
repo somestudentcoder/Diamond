fc61329256bb3a6ca903672755eceb14863975ce
Christopher Oser <christopher.oser@outlook.com>
2020-12-21 13:08:11 +0100

    changed branding

--- project/src/app/create-card-sort/create-card-sort.component.ts
+++ project/src/app/create-card-sort/create-card-sort.component.ts
@@ -32,7 +32,6 @@ export class CreateCardSortComponent implements OnInit {
   thankYouScreen = "Thank you for participation.";
   leaveFeedback = "Your results are saved. You can write us your feedback (optional).";
 
-  leafNodes = true;
   orderNumbers = true;
 
   canSave = false;
@@ -57,16 +56,11 @@ export class CreateCardSortComponent implements OnInit {
             // tslint:disable-next-line:no-angle-bracket-type-assertion
             this.testName = (res as any).name;
             this.studyPassword = (res as any).password;
-            this.createTree('test-tree', (res as any).tree);
             this.tasks = (res as any).tasks;
             this.welcomeMessage = (res as any).welcomeMessage;
             this.instructions = (res as any).instructions;
             this.thankYouScreen = (res as any).thankYouScreen;
             this.leaveFeedback = (res as any).leaveFeedback;
-            if ((res as any).leafNodes !== undefined) {
-              this.leafNodes = (res as any).leafNodes;
-              this.orderNumbers = (res as any).orderNumbers;
-            }
           },
           err => {
           }
@@ -80,21 +74,14 @@ export class CreateCardSortComponent implements OnInit {
             'selected' : true
           },}
       ];
-        this.createTree('test-tree', arrayCollection);
+        //this.createTree('test-tree', arrayCollection);
     }
-  }
-
-  open(link) {;
-    if (link === "tasks") {
-      this.defineCorrectAnswer(0);
-    }
-    $('a[href="#' + link + '"]').click();
   }
 
   getData() {
     const v = $('#test-tree').jstree(true).get_json('#', {flat: true});
     const mytext = JSON.stringify(v);
-    this.createTree('test-tree2', v);
+    //this.createTree('test-tree2', v);
   }
 
   onFileLoad(fileLoadedEvent) {
@@ -191,7 +178,7 @@ export class CreateCardSortComponent implements OnInit {
         this.itemsFinal = items;
 
         $('#test-tree').jstree("destroy").empty();
-        this.createTree('test-tree', []);
+        //this.createTree('test-tree', []);
 
         setTimeout(() => {
           for (var i = 0; i < items.length; i++) {
@@ -206,7 +193,7 @@ export class CreateCardSortComponent implements OnInit {
         }, 300);
 
         $('#test-tree-answer').jstree("destroy").empty();
-        this.createTree('test-tree-answer', []);
+        //this.createTree('test-tree-answer', []);
 
         setTimeout(() => {
           for (var i = 0; i < items.length; i++) {
@@ -227,128 +214,11 @@ export class CreateCardSortComponent implements OnInit {
 
   }
 
-  createTree(id, content) {
-    if (id === "test-tree-answer") {
-      console.log("creating tree!");
-      console.log(content);
-      //$('#test-tree-answer').jstree("destroy").empty();
-      //this.createTree('test-tree-answer', []);
+  open(link) {;
+    if (link === "tasks") {
+      //this.defineCorrectAnswer(0);
     }
-
-    if (localStorage.getItem('jstree')) {
-      localStorage.removeItem('jstree');
-    }
-    $('#' + id).jstree({
-      core : {
-        animation : 0,
-        check_callback : function test(op, node, par, pos, more) {
-            if (op === 'delete_node') {
-                if (node.parent === '#') {
-                    alert('Cannot delete root node!');
-                    return false;
-                }
-            }
-        },
-        themes : { icons: false  },
-        data : content
-      },
-      types : {
-        root : {
-          icon : '/static/3.3.7/assets/images/tree_icon.png',
-          valid_children : ['default']
-        },
-        default : {
-          valid_children : ['default', 'file']
-        },
-        file : {
-          icon : 'glyphicon glyphicon-file',
-          valid_children : []
-        }
-      },
-      plugins : [
-        'contextmenu', 'dnd', 'search',
-        'state', 'types', 'wholerow'
-      ]
-    });
-
-    //if (!this.answerTreeCreated) {
-      $('#test-tree-answer').on('select_node.jstree', (e, data) => {
-        if (!data.node.children.length) {
-          this.canSave = true;
-          this.saveAnswer();
-        }
-        else if (!this.leafNodes) {
-          this.canSave = true;
-          this.saveAnswer();
-        }
-        else this.canSave = false;;
-      });
-    //}
-    this.answerTreeCreated = true;
-    if (id === 'test-tree') {
-      $('#' + id).jstree('select_node', 'root');
-    } else if (id === 'test-tree-answer') {
-      setTimeout(() => {
-        $('#' + id).jstree("deselect_all");
-        if (this.tasks[this.currentTaskIndex] && this.tasks[this.currentTaskIndex].id) {
-        $('#' + id).jstree('select_node', this.tasks[this.currentTaskIndex].id);
-        } else {
-          $('#' + id).jstree(true).select_node('root');
-        }
-      }, 100);
-    }
-  }
-
-  addNode() {
-    const currentNode = $('#test-tree').jstree('get_selected');
-    $('#test-tree').jstree('create_node', currentNode, {text : 'new Node'}, 'last' , function test(newNode) {
-        $('#test-tree').jstree('open_node', currentNode);
-        const inst = $.jstree.reference(newNode);
-        inst.edit(newNode);
-    });
-  }
-
-  renameNode() {
-    const instance = $('#test-tree').jstree(true);
-    instance.edit(instance.get_selected());
-  }
-
-  deleteSelectedNode() {
-    const instance = $('#test-tree').jstree(true);
-    instance.delete_node(instance.get_selected());
-  }
-
-  setIndex(index) {
-    this.currentTaskIndex = index;
-    this.defineCorrectAnswer(index);
-  }
-
-  defineCorrectAnswer(index, newAnswer?) {
-      const v = $('#test-tree').jstree(true).get_json('#', {flat: true});
-      const mytext = JSON.stringify(v);
-      this.currentTaskIndex = index;
-      setTimeout(() => { this.createTree('test-tree-answer', v); }, 0);
-    //}
-  }
-
-  saveAnswer() {
-    const selected = ($('#test-tree-answer').jstree('get_selected', true))[0];
-    this.tasks[this.currentTaskIndex].answer = selected.text;
-    this.tasks[this.currentTaskIndex].id = selected.id;
-  }
-
-  answerNotValid() {
-    const selected = ($('#test-tree-answer').jstree('get_selected', true))[0];
-    if ($('#test-tree-answer').jstree('get_selected', true)) {
-      const v = $('#test-tree').jstree(true).get_json('#', {flat: true});
-      for (const el of v) {
-        if (el.parent === selected.text) {
-          return true;
-        }
-      }
-      return false;
-    }
-    return true;
+    $('a[href="#' + link + '"]').click();
   }
 
   addTask() {
@@ -383,7 +253,6 @@ export class CreateCardSortComponent implements OnInit {
         instructions: this.instructions,
         thankYouScreen: this.thankYouScreen,
         leaveFeedback: this.leaveFeedback,
-        leafNodes: this.leafNodes,
         orderNumbers: this.orderNumbers
     };
 
