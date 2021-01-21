9e34d77fd9984229c4b249d375dd0c4873f4cb7f
Markus Stradner <markus.stradner@student.tugraz.at>
2021-01-21 11:59:03 +0100

    changes paths for Heroku deployment

--- server/user-paths.js
+++ server/user-paths.js
@@ -19,7 +19,7 @@ server_url: 'https://iaweb-diamond.herokuapp.com',
 // db_connection_url: 'mongodb://localhost:27017/node-mongo-registration-login-api',
 
 // Heroku
-db_connection_url: "mongodb+srv://admin:admin189m@iaweb-test-suite.kvxk0.mongodb.net/iaweb-test-suite?retryWrites=true&w=majority",
+db_connection_url: "mongodb+srv://admin:iaweb-admin2021@iaweb-diamond.kvxk0.mongodb.net/iaweb-diamond?retryWrites=true&w=majority",
 
 }
 
